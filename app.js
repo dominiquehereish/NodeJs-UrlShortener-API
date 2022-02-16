ac682f4757ea56ea3d71abf9e8fa20d96a65d33b
unknown <ha291921@ens.uqam.ca>
2022-02-16 14:31:45 -0500

    main app

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
-
+const router = require('./rest/shortener_router')
 const db = require('./database/db')
+const urls = require('./models/urls')
 
 const app = express()
 
+app.use(express.json())
+app.use('/api',router)
+
+db.connect()
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
