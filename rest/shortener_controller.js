c11a5c3c5b6f1d01888c1eaf6675662b82674b7a
unknown <ha291921@ens.uqam.ca>
2022-02-16 14:23:32 -0500

    shortener controller

--- rest/shortener_controller.js
+++ rest/shortener_controller.js
@@ -3,6 +3,25 @@ const urls = require('../models/urls')
 
 class ShortenerController{
 
+    async index (req, res) {
+        res.status(200).json({message: 'Welcome to my url shortener api.'})
+    }
+
+    async getAll (req, res) {
+        res.status(200).json({})
+    }
+
+    async getOne (req, res) {
+        res.status(200).json({})
+    }
+
+    async createOne (req, res) {
+        res.status(200).json({})
+    }
+
+    async delete (req, res) {
+        res.status(200).json({})
+    }
 }
 
 module.exports = new ShortenerController()
